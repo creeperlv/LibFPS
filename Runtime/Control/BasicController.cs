5410309023dcb288a3439c6ec9d2c3dbf1485a12
Creeper Lv <creeperlv@users.noreply.github.com>
2024-04-10 02:31:10 +1000

    Update BasicController.cs
    It should be a local only script.

--- Runtime/Control/BasicController.cs
+++ Runtime/Control/BasicController.cs
@@ -1,28 +1,36 @@
+using Mirror;
 using UnityEngine;
 
-namespace LibFPS
+namespace LibFPS.Control
 {
-    public class BasicController : MonoBehaviour
+    public class BasicController : NetworkBehaviour
     {
+        public bool IsMovementInUse;
+        public bool IsLookInUse;
         public float MoveSpeed;
+        public GameObject LocalPlayerOnlyObjects;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
+            LocalPlayerOnlyObjects.SetActive(isLocalPlayer);
         }
 
         // Update is called once per frame
         void Update()
         {
-            var v = Input.GetAxis("Vertical");
-            if (Mathf.Abs(v) > 0.05f)
+            if (!isLocalPlayer) { return; }
+            if (IsMovementInUse)
             {
-                this.transform.Translate(Vector3.forward * v * MoveSpeed, Space.Self);
-            }
-            var h = Input.GetAxis("Horizontal");
-            if (Mathf.Abs(h) > 0.05f)
-            {
-                this.transform.Translate(Vector3.right * h * MoveSpeed, Space.Self);
+                var v = Input.GetAxis("Vertical");
+                if (Mathf.Abs(v) > 0.05f)
+                {
+                    this.transform.Translate(Vector3.forward * v * MoveSpeed, Space.Self);
+                }
+                var h = Input.GetAxis("Horizontal");
+                if (Mathf.Abs(h) > 0.05f)
+                {
+                    this.transform.Translate(Vector3.right * h * MoveSpeed, Space.Self);
+                }
             }
         }
     }
