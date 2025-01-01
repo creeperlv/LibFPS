a906ce6b9dee3609179571a35b8b46214d699b9e
Creeper Lv <creeperlv@users.noreply.github.com>
2025-01-02 02:51:36 +1100

    Biped now supports setting directional bools in Animators.

--- Runtime/AnimationSystem/WeightedRotationSync.cs
+++ Runtime/AnimationSystem/WeightedRotationSync.cs
@@ -11,17 +11,11 @@ namespace LibFPS.AnimationSystem
 		public Transform Target;
 		public Vector3 Weight;
 		public float fWeight;
-		public Vector3 Debug0;
-		public Vector3 Debug1;
-		public Vector3 Debug2;
 		public void Update()
 		{
 			Vector3 srcRot = Source.localEulerAngles.ToZeroCenteredRotation();
-			Debug0 = srcRot;
 			Vector3 calced = new Vector3(srcRot.x * Weight.x, srcRot.y * Weight.y, srcRot.z * Weight.z);
-			Debug1 = calced;
 			Target.localEulerAngles = calced;
-			Debug2 = Target.localEulerAngles;
 		}
 	}
 }
--- Runtime/Gameplay/Biped.cs
+++ Runtime/Gameplay/Biped.cs
@@ -13,6 +13,10 @@ namespace LibFPS.Gameplay
 		public float RunSpeed;
 		public string AnimatorWalking = "IsWalking";
 		public string AnimatorRunning = "IsRunning";
+		public string Forward = "Forward";
+		public string Backward = "Backward";
+		public string Leftward = "Leftward";
+		public string Rightward = "Rightward";
 		public bool IsRunning;
 		public void Move(float h, float v)
 		{
@@ -20,13 +24,17 @@ namespace LibFPS.Gameplay
 			{
 				UpperAnimator.SetBool(AnimatorWalking, false);
 				UpperAnimator.SetBool(AnimatorRunning, false);
+				LowerAnimator.SetBool(AnimatorWalking, false);
+				LowerAnimator.SetBool(AnimatorRunning, false);
 			}
 			else
 			{
 				UpperAnimator.SetBool(AnimatorWalking, true);
 				UpperAnimator.SetBool(AnimatorRunning, IsRunning);
+				LowerAnimator.SetBool(AnimatorWalking, true);
+				LowerAnimator.SetBool(AnimatorRunning, IsRunning);
 			}
-			var t = CharacterController.transform;
+			var t = Self;
 			if (IsRunning)
 			{
 				h *= RunSpeed;
@@ -37,7 +45,42 @@ namespace LibFPS.Gameplay
 				h *= WalkSpeed;
 				v *= WalkSpeed;
 			}
+			var av = Mathf.Abs(v);
+			var ah = Mathf.Abs(h);
+			if (av > ah)
+			{
+				if (v > 0)
+				{
+					SetDirection(UpperAnimator, true, false, false, false);
+					SetDirection(LowerAnimator, true, false, false, false);
+				}
+				else
+				{
+					SetDirection(UpperAnimator, false, true, false, false);
+					SetDirection(LowerAnimator, false, true, false, false);
+				}
+			}
+			else
+			{
+				if (h > 0)
+				{
+					SetDirection(UpperAnimator, false, false, false, true);
+					SetDirection(LowerAnimator, false, false, false, true);
+				}
+				else
+				{
+					SetDirection(UpperAnimator, false, false, true, false);
+					SetDirection(LowerAnimator, false, false, true, false);
+				}
+			}
 			CharacterController.SimpleMove(t.forward * v + t.right * h + t.up * -10);
+		}
+		public void SetDirection(Animator animator, bool FWD, bool BWD, bool LWD, bool RWD)
+		{
+			animator.SetBool(Forward, FWD);
+			animator.SetBool(Backward, BWD);
+			animator.SetBool(Leftward, LWD);
+			animator.SetBool(Rightward, RWD);
 		}
 	}
 }
