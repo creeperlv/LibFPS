3f5e6167d2d7e3cfbc7ff0c1a1184641dd64617c
Creeper Lv <creeperlv@users.noreply.github.com>
2025-02-03 04:50:36 +1100

    Started working on the network session manager.

---
+++ Runtime/Networking/NetworkSessionManager.cs
@@ -0,0 +1,29 @@
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+
+namespace LibFPS.Networking
+{
+	public class NetworkSessionManager : MonoBehaviour
+	{
+		public NetworkManager NetworkManager;
+		public UnityTransport transport;
+		public IConnectionDataProvider connectionDataProvider;
+		public void CreateLobby(ushort Port, bool RequireApproval)
+		{
+			NetworkManager.NetworkConfig.ConnectionApproval = RequireApproval;
+			transport.ConnectionData.Port = Port;
+			NetworkManager.StartHost();
+		}
+		public void ConnectToLobby(string Address, ushort Port)
+		{
+			transport.ConnectionData.Address = Address;
+			transport.ConnectionData.Port = Port;
+			NetworkManager.StartClient();
+		}
+	}
+	public interface IConnectionDataProvider
+	{
+		string GetDisplayName();
+	}
+}
