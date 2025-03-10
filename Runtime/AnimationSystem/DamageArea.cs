26d8b49f5e9ade7e997b9add8e692b494136d016
Creeper Lv <creeperlv@users.noreply.github.com>
2025-03-11 09:41:08 +1100

    Update DamageArea.cs

--- Runtime/AnimationSystem/DamageArea.cs
+++ Runtime/AnimationSystem/DamageArea.cs
@@ -8,16 +8,19 @@ namespace LibFPS.AnimationSystem
 	{
 		public BaseEntity Giver;
 		public DamageConfig damage;
+		public bool DealOverTime;
 		public void OnTriggerStay(Collider other)
 		{
+			if (!DealOverTime) return;
 			var be = other.GetComponentInParent<BaseEntity>();
 			if (be != Giver)
 			{
-				be.DealDamage(damage);
+				be.DealDamageDT(damage);
 			}
 		}
 		public void OnTriggerEnter(Collider other)
 		{
+			if (DealOverTime) return;
 			var be = other.GetComponentInParent<BaseEntity>();
 			if (be != Giver)
 			{
